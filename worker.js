a415af0f4aa9aa56b9a07503661c0345e451ce7b
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2016-03-05 11:37:25 +0100

    it works now

--- worker.js
+++ worker.js
@@ -1,10 +1,13 @@
-function onmessage(msg) {
+importScripts('sjcl.min.js')
+
+onmessage = e => {
+  var msg = e.data
   var res = getHash(msg[0], msg[1])
-  sendMessage(res)
+  postMessage(res)
 }
 
-function getHash(seed, salt){
-  var data = sjcl.misc.pbkdf2(seed, salt, Math.pow(2,64), 32 * 4)
+function getHash (seed, salt) {
+  var data = sjcl.misc.pbkdf2(seed, salt, Math.pow(2, 16), 32 * 4)
   var res = sjcl.codec.hex.fromBits(data)
   return res
 }
